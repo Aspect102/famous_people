007cb0dbb22aa7adcc808611baaadeb78eefc6f3
Aspect102 <53630875+Aspect102@users.noreply.github.com>
2022-09-22 16:24:21 +0100

    Add files via upload

---
+++ Famous People/Famous People/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_Turing = New System.Windows.Forms.Button()
+        Me.btn_Berners = New System.Windows.Forms.Button()
+        Me.btn_Hopper = New System.Windows.Forms.Button()
+        Me.btn_Babbage = New System.Windows.Forms.Button()
+        Me.btn_Shannon = New System.Windows.Forms.Button()
+        Me.lbl_Photo = New System.Windows.Forms.Label()
+        Me.pbx_Photo = New System.Windows.Forms.PictureBox()
+        Me.lbl_Facts = New System.Windows.Forms.Label()
+        Me.txt_Facts = New System.Windows.Forms.TextBox()
+        Me.lbl_Born = New System.Windows.Forms.Label()
+        Me.lbl_Died = New System.Windows.Forms.Label()
+        Me.txt_Born = New System.Windows.Forms.TextBox()
+        Me.txt_Died = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.lbl_Title = New System.Windows.Forms.Label()
+        CType(Me.pbx_Photo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_Turing
+        '
+        Me.btn_Turing.Location = New System.Drawing.Point(53, 136)
+        Me.btn_Turing.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_Turing.Name = "btn_Turing"
+        Me.btn_Turing.Size = New System.Drawing.Size(128, 65)
+        Me.btn_Turing.TabIndex = 0
+        Me.btn_Turing.Text = "Alan Turing"
+        Me.btn_Turing.UseVisualStyleBackColor = True
+        '
+        'btn_Berners
+        '
+        Me.btn_Berners.Location = New System.Drawing.Point(53, 227)
+        Me.btn_Berners.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_Berners.Name = "btn_Berners"
+        Me.btn_Berners.Size = New System.Drawing.Size(128, 65)
+        Me.btn_Berners.TabIndex = 1
+        Me.btn_Berners.Text = "Tim Berners-Lee"
+        Me.btn_Berners.UseVisualStyleBackColor = True
+        '
+        'btn_Hopper
+        '
+        Me.btn_Hopper.Location = New System.Drawing.Point(53, 309)
+        Me.btn_Hopper.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_Hopper.Name = "btn_Hopper"
+        Me.btn_Hopper.Size = New System.Drawing.Size(128, 65)
+        Me.btn_Hopper.TabIndex = 2
+        Me.btn_Hopper.Text = "Grace Hopper"
+        Me.btn_Hopper.UseVisualStyleBackColor = True
+        '
+        'btn_Babbage
+        '
+        Me.btn_Babbage.Location = New System.Drawing.Point(53, 396)
+        Me.btn_Babbage.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_Babbage.Name = "btn_Babbage"
+        Me.btn_Babbage.Size = New System.Drawing.Size(128, 65)
+        Me.btn_Babbage.TabIndex = 3
+        Me.btn_Babbage.Text = "Charles Babbage"
+        Me.btn_Babbage.UseVisualStyleBackColor = True
+        '
+        'btn_Shannon
+        '
+        Me.btn_Shannon.Location = New System.Drawing.Point(53, 485)
+        Me.btn_Shannon.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_Shannon.Name = "btn_Shannon"
+        Me.btn_Shannon.Size = New System.Drawing.Size(128, 65)
+        Me.btn_Shannon.TabIndex = 4
+        Me.btn_Shannon.Text = "Claude Shannon"
+        Me.btn_Shannon.UseVisualStyleBackColor = True
+        '
+        'lbl_Photo
+        '
+        Me.lbl_Photo.AutoSize = True
+        Me.lbl_Photo.Location = New System.Drawing.Point(279, 96)
+        Me.lbl_Photo.Name = "lbl_Photo"
+        Me.lbl_Photo.Size = New System.Drawing.Size(48, 20)
+        Me.lbl_Photo.TabIndex = 5
+        Me.lbl_Photo.Text = "Photo"
+        '
+        'pbx_Photo
+        '
+        Me.pbx_Photo.Location = New System.Drawing.Point(279, 131)
+        Me.pbx_Photo.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.pbx_Photo.Name = "pbx_Photo"
+        Me.pbx_Photo.Size = New System.Drawing.Size(234, 244)
+        Me.pbx_Photo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbx_Photo.TabIndex = 6
+        Me.pbx_Photo.TabStop = False
+        '
+        'lbl_Facts
+        '
+        Me.lbl_Facts.AutoSize = True
+        Me.lbl_Facts.Location = New System.Drawing.Point(558, 96)
+        Me.lbl_Facts.Name = "lbl_Facts"
+        Me.lbl_Facts.Size = New System.Drawing.Size(73, 20)
+        Me.lbl_Facts.TabIndex = 7
+        Me.lbl_Facts.Text = "Facts/Info"
+        '
+        'txt_Facts
+        '
+        Me.txt_Facts.Location = New System.Drawing.Point(558, 131)
+        Me.txt_Facts.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_Facts.Multiline = True
+        Me.txt_Facts.Name = "txt_Facts"
+        Me.txt_Facts.Size = New System.Drawing.Size(314, 329)
+        Me.txt_Facts.TabIndex = 8
+        '
+        'lbl_Born
+        '
+        Me.lbl_Born.AutoSize = True
+        Me.lbl_Born.Location = New System.Drawing.Point(279, 424)
+        Me.lbl_Born.Name = "lbl_Born"
+        Me.lbl_Born.Size = New System.Drawing.Size(40, 20)
+        Me.lbl_Born.TabIndex = 9
+        Me.lbl_Born.Text = "Born"
+        '
+        'lbl_Died
+        '
+        Me.lbl_Died.AutoSize = True
+        Me.lbl_Died.Location = New System.Drawing.Point(410, 424)
+        Me.lbl_Died.Name = "lbl_Died"
+        Me.lbl_Died.Size = New System.Drawing.Size(41, 20)
+        Me.lbl_Died.TabIndex = 10
+        Me.lbl_Died.Text = "Died"
+        '
+        'txt_Born
+        '
+        Me.txt_Born.Location = New System.Drawing.Point(269, 465)
+        Me.txt_Born.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_Born.Name = "txt_Born"
+        Me.txt_Born.Size = New System.Drawing.Size(102, 27)
+        Me.txt_Born.TabIndex = 11
+        '
+        'txt_Died
+        '
+        Me.txt_Died.Location = New System.Drawing.Point(410, 465)
+        Me.txt_Died.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txt_Died.Name = "txt_Died"
+        Me.txt_Died.Size = New System.Drawing.Size(102, 27)
+        Me.txt_Died.TabIndex = 12
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(878, 40)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(86, 31)
+        Me.Button6.TabIndex = 13
+        Me.Button6.Text = "Clear"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'lbl_Title
+        '
+        Me.lbl_Title.AutoSize = True
+        Me.lbl_Title.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_Title.Location = New System.Drawing.Point(53, 11)
+        Me.lbl_Title.Name = "lbl_Title"
+        Me.lbl_Title.Size = New System.Drawing.Size(776, 60)
+        Me.lbl_Title.TabIndex = 14
+        Me.lbl_Title.Text = "Famous People In Computer Science"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1003, 600)
+        Me.Controls.Add(Me.lbl_Title)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.txt_Died)
+        Me.Controls.Add(Me.txt_Born)
+        Me.Controls.Add(Me.lbl_Died)
+        Me.Controls.Add(Me.lbl_Born)
+        Me.Controls.Add(Me.txt_Facts)
+        Me.Controls.Add(Me.lbl_Facts)
+        Me.Controls.Add(Me.pbx_Photo)
+        Me.Controls.Add(Me.lbl_Photo)
+        Me.Controls.Add(Me.btn_Shannon)
+        Me.Controls.Add(Me.btn_Babbage)
+        Me.Controls.Add(Me.btn_Hopper)
+        Me.Controls.Add(Me.btn_Berners)
+        Me.Controls.Add(Me.btn_Turing)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Famous People In Computer Science"
+        CType(Me.pbx_Photo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_Turing As Button
+    Friend WithEvents btn_Berners As Button
+    Friend WithEvents btn_Hopper As Button
+    Friend WithEvents btn_Babbage As Button
+    Friend WithEvents btn_Shannon As Button
+    Friend WithEvents lbl_Photo As Label
+    Friend WithEvents pbx_Photo As PictureBox
+    Friend WithEvents lbl_Facts As Label
+    Friend WithEvents txt_Facts As TextBox
+    Friend WithEvents lbl_Born As Label
+    Friend WithEvents lbl_Died As Label
+    Friend WithEvents txt_Born As TextBox
+    Friend WithEvents txt_Died As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents lbl_Title As Label
+End Class
---
+++ Famous People/Famous People/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim pathname As String 'define pathname
+
+    Function PopulateFields(person As String)
+        Dim fileReader As System.IO.StreamReader, filereaderpath As String
+        filereaderpath = pathname
+        Dim Turing, Berners, Hopper, Babbage, Shannon
+        Using reader As System.IO.StreamReader
+            'in progress
+        End Using
+        If person = "Turing" Then
+            Turing =
+            txt_Facts.Text = ""
+            pbx_Photo.ImageLocation = pathname & "turing.jfif"
+            txt_Born.Text = "23 June 1912"
+            txt_Died.Text = "7 June 1954"
+        ElseIf person = "Berners" Then
+            Berners = My.Computer.FileSystem.ReadAllText(filereaderpath & "berners.txt")
+            pbx_Photo.ImageLocation = pathname & "berners.jfif"
+            txt_Born.Text = "8 June 1955"
+            txt_Died.Text = "N/A"
+            txt_Facts.Text = ""
+        ElseIf person = "Hopper" Then
+            Hopper = My.Computer.FileSystem.ReadAllText(filereaderpath & "hopper.txt")
+            pbx_Photo.ImageLocation = pathname & "hopper.jpg"
+            txt_Born.Text = "9 December 1906"
+            txt_Died.Text = "1 January 1992"
+            txt_Facts.Text = ""
+        ElseIf person = "Babbage" Then
+            Babbage = My.Computer.FileSystem.ReadAllText(filereaderpath & "babbage.txt")
+            pbx_Photo.ImageLocation = pathname & "babbage.jpg"
+            txt_Born.Text = "26 December 1791"
+            txt_Died.Text = "18 October 1871"
+            txt_Facts.Text = ""
+        ElseIf person = "Shannon" Then
+            Shannon = My.Computer.FileSystem.ReadAllText(filereaderpath & "shannon.txt")
+            pbx_Photo.ImageLocation = pathname & "shannon.jpg"
+            txt_Born.Text = "30 April 1916"
+            txt_Died.Text = "24 February 2001"
+            txt_Facts.Text = ""
+        End If
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox(pathname)
+        pathname = Application.StartupPath 'set pathname to current pathname
+    End Sub
+
+    Private Sub btn_Turing_Click(sender As Object, e As EventArgs) Handles btn_Turing.Click
+        PopulateFields("Turing")
+    End Sub
+
+    Private Sub btn_Berners_Click(sender As Object, e As EventArgs) Handles btn_Berners.Click
+        PopulateFields("Berners")
+    End Sub
+
+    Private Sub btn_Hopper_Click(sender As Object, e As EventArgs) Handles btn_Hopper.Click
+        PopulateFields("Hopper")
+    End Sub
+
+    Private Sub btn_Babbage_Click(sender As Object, e As EventArgs) Handles btn_Babbage.Click
+        PopulateFields("Babbage")
+    End Sub
+
+    Private Sub btn_Shannon_Click(sender As Object, e As EventArgs) Handles btn_Shannon.Click
+        PopulateFields("Shannon")
+    End Sub
+End Class
